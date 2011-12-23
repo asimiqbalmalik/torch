07ed82488645153c3c35f2016cb759985870e660
mcdonough.colin <mcdonough.colin@gmail.com>
2011-12-23 23:33:18 +0000

    Removed logging for release

--- src/com/colinmcdonough/android/torch/QuickTorch.java
+++ src/com/colinmcdonough/android/torch/QuickTorch.java
@@ -2,11 +2,8 @@ package com.colinmcdonough.android.torch;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
 
 public class QuickTorch extends Activity {
-
-  private static final String TAG = QuickTorch.class.getSimpleName();
   
   /**
    * Start Torch when triggered
@@ -14,13 +11,10 @@ public class QuickTorch extends Activity {
   @Override
   public void onStart() {
     super.onStart();
-    Log.d(TAG, "onStart()");
     if (Torch.getTorch() == null) {
-      Log.d(TAG, "torch == null");
       Intent intent = new Intent(this, Torch.class);
       startActivity(intent);
     } else {
-      Log.d(TAG, "torch != null");
     }
     finish();
   }
